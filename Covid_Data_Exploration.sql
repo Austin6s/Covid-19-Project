922dd76c0e83bd72648597afe65638b7897bd627
Austin Rohlfing <austin.h.rohlfing@gmail.com>
2021-09-10 18:00:46 -0400

    feat: Add more queries
    Added several queries to analyze rate of infections and death rates by country.

--- Covid_Data_Exploration.sql
+++ Covid_Data_Exploration.sql
@@ -3,7 +3,7 @@ WITH working_tbl AS (
 	SELECT location, date, total_cases :: float, new_cases, total_deaths :: float, population
 	FROM deaths
 	ORDER BY 1,2)
-	
+
 --Shows likelihood of dying from Covid in the United States
 SELECT location, date, total_cases, total_deaths, total_deaths/total_cases AS chance_of_death
 FROM working_tbl
@@ -11,7 +11,28 @@ WHERE location like 'United States'
 ORDER BY 2
 
 --Show percentage of people with Covid in the United States
-SELECT date, population, total_cases, total_cases/population AS chance_of_contraction
+SELECT date, population, total_cases, total_cases/population AS infection_rate
 FROM working_tbl
 WHERE location LIKE 'United States'
 ORDER BY 1
+
+--Average rate of infection per country
+SELECT location, AVG(total_cases/population) infection_rate
+FROM working_tbl
+GROUP BY 1
+HAVING AVG(total_cases/population) IS NOT NULL
+ORDER BY 1
+
+--Countries with highest peak infection rate
+SELECT location, MAX(total_cases/population) infection_rate
+FROM working_tbl
+GROUP BY 1
+HAVING AVG(total_cases/population) IS NOT NULL
+ORDER BY 2 desc
+
+--Countries with highest peak Covid death rate per population
+SELECT location, MAX(total_deaths/population) covid_death_rate
+FROM working_tbl
+GROUP BY 1
+HAVING MAX(total_deaths/population) IS NOT NULL
+ORDER BY 2 desc
