581435b755d59e2a968812dce2a63896e155362c
Austin Rohlfing <austin.h.rohlfing@gmail.com>
2021-09-10 16:07:54 -0400

    Update Covid_Data_Exploration.sql

--- Covid_Data_Exploration.sql
+++ Covid_Data_Exploration.sql
@@ -8,9 +8,10 @@ WITH working_tbl AS (
 SELECT location, date, total_cases, total_deaths, total_deaths/total_cases AS chance_of_death
 FROM working_tbl
 WHERE location like 'United States'
-ORDER BY 2;
+ORDER BY 2
 
 --Show percentage of people with Covid in the United States
 SELECT date, population, total_cases, total_cases/population
 FROM working_tbl
 WHERE location LIKE 'United States'
+ORDER BY 1
