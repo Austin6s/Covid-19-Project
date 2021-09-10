967d61d06c28db3f3f57d43a0f37f59a48c5f744
Austin Rohlfing <austin.h.rohlfing@gmail.com>
2021-09-10 16:14:21 -0400

    style: Change total_cases to float

--- Covid_Data_Exploration.sql
+++ Covid_Data_Exploration.sql
@@ -1,6 +1,6 @@
 --First create a table with data we want to use throughout this project
 WITH working_tbl AS (
-	SELECT location, date, total_cases, new_cases, total_deaths :: float, population
+	SELECT location, date, total_cases :: float, new_cases, total_deaths :: float, population
 	FROM deaths
 	ORDER BY 1,2)
 	
@@ -11,7 +11,7 @@ WHERE location like 'United States'
 ORDER BY 2
 
 --Show percentage of people with Covid in the United States
-SELECT date, population, total_cases, total_cases/population
+SELECT date, population, total_cases, total_cases/population AS chance_of_contraction
 FROM working_tbl
 WHERE location LIKE 'United States'
 ORDER BY 1
